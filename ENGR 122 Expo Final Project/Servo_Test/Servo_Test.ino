e71a877032cab3386dfd6780a3d3485b343f9cd1
jameswbrooksiii <92341441+jameswbrooksiii@users.noreply.github.com>
2021-10-12 13:07:34 -0500

    Update Servo_Test.ino

--- ENGR 122 Expo Final Project/Servo_Test/Servo_Test.ino
+++ ENGR 122 Expo Final Project/Servo_Test/Servo_Test.ino
@@ -1,3 +1,7 @@
+// A simple test script using an isolated circuit only containing the pressure pad
+// and servo were communicating with the Arduino correctly and working properly.
+
+// Setup
 int PressurePin = A0;
 int force;
 
@@ -7,7 +11,8 @@ void setup()
 }
 void loop()
 {
-  force = analogRead(PressurePin);
-  Serial.println(force);
-  
+  force = analogRead(PressurePin); // Read the pressure being applied to the pressure pad
+  Serial.println(force); // Print the pressure applied as a numeric value
+                         // This value was used to determine the thresholds of when the 
+                         // servo should engage/disengage.
 }
